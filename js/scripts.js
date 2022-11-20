795dcdf44d33a1fc0929a981b96787998adcf469
Charles Cookman <CharlesBCookman@gmail.com>
2022-11-20 08:37:21 -0800

    Basic functionality working.

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,37 @@
-const robotInput = document.getElementById("number-input");
-const robotOutput = document.getElementById("robot-output");
-
-
-let numberAll;
-let outputCollector = [];
 function outputButton(){
-  for(numberAll = 0; numberAll < parseInt(robotInput.value); numberAll++ ){
-    if(isNaN(parseInt(robotInput.value)) === true) {
-      alert("Your value isn't working.")
-    } else if (robotInput.value.includes("3")) {
-      outputCollector.push(["Won't you be my neighbor?"]);
-    } else if(robotInput.value.includes("2")) {
-      outputCollector.push(["Boop!"]);
-    } else if(robotInput.value.includes("1")) {
-      outputCollector.push(["Beep!"]);
+  const robotOutput = document.getElementById("robot-output");
+  const robotInput = document.getElementById("number-input");
+  let outputCollector = [];
+  const numberCollected = parseInt(robotInput.value);
+  console.log(numberCollected)
+  for(i = 0; i < parseInt(numberCollected); i++ ){
+    console.log(i);
+    console.log(outputCollector);
+    if(isNaN(numberCollected) === true) {
+      alert("Your value is NaN.");
+      console.log("test if value is number");
+    } else if (i.toString().includes("3")) {
+      outputCollector.push([" Won't you be my neighbor?"]);
+      console.log("second test");
+    } else if(i.toString().includes("2")) {
+      outputCollector.push([" Boop!"]);
+      console.log("third test");
+    } else if(i.toString().includes("1")) {
+      outputCollector.push([" Beep!"]);
+      console.log("fourth test");
+    } else if (i === 0) {
+      outputCollector.push(i);
+      console.log("fifth test");
     } else if (true === true) {
-      outputCollector.push([numberAll]);
+      outputCollector.push(" " + i);
+      console.log("fifth test");
     }
   }
-  outputCollector.forEach(function(element) {
-    robotOutput.value.push(element);
-  });
+  robotOutput.value = outputCollector.toString();
 }
+
+window.addEventListener("load", function(){
+  const robotButton = document.getElementById("robot-button");
+  console.log("Load sequence initiated.");
+  robotButton.addEventListener("click", outputButton);
+});
