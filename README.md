a9946da7ae3450df7dea71b6cb037c16175247e0
Charles Cookman <CharlesBCookman@gmail.com>
2022-11-20 08:56:58 -0800

    Removed  console.log's added testing method to README.md.

---
+++ README.md
@@ -0,0 +1,21 @@
+Describe: outputButton()
+
+Test: "It takes a value grabbed from the input and returns every number from 0 to that number."
+Code: output Button(42);
+Expected Output: [0, 1, 2, 3, 4, 5, 6, ... 42]
+
+Test: "it takes every value with a 1 in it and replaces it with "Beep!".
+Code: output Button(5);
+Expected Output: [0, Beep!, 2, 3, 4, 5]
+
+Test: "It takes every value with a 2 in it and replaces it with "Boop!".
+Code: output Button(5);
+Expected Output: [0, Beep!, Boop!, 3, 4, 5]
+
+Test: "It takes every value with a 3 in it and replaces it with "Wont you be my neightbor".
+Code: output Button(5);
+Expected Output: [0, Beep!, Boop!, Wont you be my neightbor, 4, 5]
+
+Test: "It takes every value with a 3 in it and replaces it with "Wont you be my neightbor".
+Code: output Button(5);
+Expected Output: [0, Beep!, Boop!, 3, 4, 5]
--- js/scripts.js
+++ js/scripts.js
@@ -5,26 +5,16 @@ function outputButton(){
   const numberCollected = parseInt(robotInput.value);
   console.log(numberCollected)
   for(i = 0; i < parseInt(numberCollected); i++ ){
-    console.log(i);
-    console.log(outputCollector);
-    if(isNaN(numberCollected) === true) {
-      alert("Your value is NaN.");
-      console.log("test if value is number");
-    } else if (i.toString().includes("3")) {
+    if (i.toString().includes("3")) {
       outputCollector.push([" Won't you be my neighbor?"]);
-      console.log("second test");
     } else if(i.toString().includes("2")) {
       outputCollector.push([" Boop!"]);
-      console.log("third test");
     } else if(i.toString().includes("1")) {
       outputCollector.push([" Beep!"]);
-      console.log("fourth test");
     } else if (i === 0) {
       outputCollector.push(i);
-      console.log("fifth test");
     } else if (true === true) {
       outputCollector.push(" " + i);
-      console.log("fifth test");
     }
   }
   robotOutput.value = outputCollector.toString();
