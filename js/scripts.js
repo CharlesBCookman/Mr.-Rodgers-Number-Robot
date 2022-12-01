73d78c5e2e6e4a02a8be325dca73f55215db2178
Charles Cookman <CharlesBCookman@gmail.com>
2022-12-01 13:14:40 -0800

    setting console logs for testing as the condition of inputting a NaN doesn't seem to show the text.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,26 @@
-function outputButton(){
-  const robotOutput = document.getElementById("robot-output");
-  const robotInput = document.getElementById("number-input");
+// function outputButton(){
+//   const robotOutput = document.getElementById("robot-output");
+//   const robotInput = document.getElementById("number-input");
+//   let outputCollector = [];
+//   const numberCollected = parseInt(robotInput.value);
+//   for(i = 0; i <= parseInt(numberCollected); i++ ){
+//     if (i.toString().includes("3")) {
+//       outputCollector.push([" Won't you be my neighbor?"]);
+//     } else if(i.toString().includes("2")) {
+//       outputCollector.push([" Boop!"]);
+//     } else if(i.toString().includes("1")) {
+//       outputCollector.push([" Beep!"]);
+//     } else if (i === 0) {
+//       outputCollector.push(i);
+//     } else if (true === true) {
+//       outputCollector.push(" " + i);
+//     }
+//   }
+//   robotOutput.value = outputCollector.toString();
+// }
+function robotWork(number) {
   let outputCollector = [];
-  const numberCollected = parseInt(robotInput.value);
-  for(i = 0; i < parseInt(numberCollected); i++ ){
+  for(i = 0; i <= number; i++){
     if (i.toString().includes("3")) {
       outputCollector.push([" Won't you be my neighbor?"]);
     } else if(i.toString().includes("2")) {
@@ -16,8 +33,20 @@ function outputButton(){
       outputCollector.push(" " + i);
     }
   }
-  robotOutput.value = outputCollector.toString();
+  document.getElementById("robot-output").value = outputCollector;
 }
+
+function outputButton() {
+  let robotOutput = document.getElementById("robot-output").value;
+  let robotInput = document.getElementById("number-input").value;
+  console.log(parseInt(robotInput));
+  if (parseInt(robotInput) === NaN) {
+    robotOutput = "This is not a number.";
+  } else { 
+    robotWork(parseInt(robotInput));
+  }
+}
+
 
 window.addEventListener("load", function(){
   const robotButton = document.getElementById("robot-button");
